8acf110e93f96dd57aac5848f3068578adad39ff
astorniolo <astorniolo@gmail.com>
2023-09-18 19:09:34 -0300

    finalizado modelos, panel admin , clienteadmin con filtros y  ordenaniento

--- HolaMundo/gestorPedidos/admin.py
+++ HolaMundo/gestorPedidos/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from gestorPedidos.models import Cliente,Articulos,Pedidos
 
 # Register your models here.
-admin.site.register(Cliente)
+
+# Para elegir atributos  debo heredar de models.admin
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=("nombre","email")
+    search_fields=("nombre","email")
+    #readonly_fields=("fechaUltimaModificacion")
+    ordering=("nombre","email")
+    list_filter=("direccion",)
+    
+
+    
+admin.site.register(Cliente,ClienteAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
