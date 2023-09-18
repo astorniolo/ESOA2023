0957def78998aa8a624ca0b85b0f54ca51d67e26
astorniolo <astorniolo@gmail.com>
2023-09-18 02:27:26 -0300

    logrado panel de administracion

--- HolaMundo/gestorPedidos/admin.py
+++ HolaMundo/gestorPedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gestorPedidos.models import Cliente,Articulos,Pedidos
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
--- HolaMundo/gestorPedidos/models.py
+++ HolaMundo/gestorPedidos/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+# crearemos una clase por tabla todas heredando de Model
+
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=30, verbose_name="Nombre y Apellido")
+    direccion=models.CharField(max_length=100)
+    email=models.EmailField(blank=True, null=True)
+    telefono=models.CharField(max_length=15)
+    
+    def __str__(self) -> str:
+        return self.nombre + ' - ' + self.direccion
+
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.nombre + ' - ' + self.seccion + ' - ' + str(self.precio)
+    
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
+    #agrego el campo fecha de entrega 
+    #y ademas le digo que puede ser en blanco o nulo
+    fechaEntrega=models.DateField(blank=True,null=True)
+    
+    def __str__(self) -> str:
+        if self.entregado:
+            return str(self.numero) + '- Entregado el ' + str(self.fecha)
+        else:   
+            return str(self.numero) + '- NO Entregado'
